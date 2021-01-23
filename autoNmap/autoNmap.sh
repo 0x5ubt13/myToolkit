5b8e2b4253b55f4170e1d33c7a8394ddcefde594
0x5ubt13 <5ubt13@protonmail.com>
2021-01-23 13:05:20 +0000

    now it prints out the settings in the name

--- autoNmap/autoNmap.sh
+++ autoNmap/autoNmap.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-echo "Welcome to autoNmap for quickly scan your box, by 0x5ubt13!"
+echo "Welcome to autoNmap for quickly scanning your box, by 0x5ubt13!"
 
 read -rp "IP to attack: " IP
-read -rp "Name of the box: " NAME
+read -rp "Name of the box to name the file: " NAME
 
 echo "[+] Scanning for open ports..."
 PORTS=$(nmap -p- --min-rate=1000 -T4 "$IP" | grep "^[0-9]" | cut -d '/' -f 1 | tr '\n' ',' | sed "s/,$//")
 
-printf "[!] Done!\nHere is a list of the ports you will be messing with this time:\n"
+printf "[!] Done!\nHere is a summary of the ports you will be messing with this time:\n"
 
 for PORT in $PORTS
 do 
-    echo "Port '$PORT'"
+    echo "'$PORT'"
 done
 
 while true ; do
@@ -21,9 +21,11 @@ while true ; do
 
     if [ "$AGGRESIVE" == "yes" ] || [ "$AGGRESIVE" == "y" ]; then
         AGGRESIVE_FLAG="-A"
+        COMP_NAME="_aggresive"
         break;
     elif [ "$AGGRESIVE" == "no" ] || [ "$AGGRESIVE" == "n" ]; then
         AGGRESIVE_FLAG="-sS"
+        COMP_NAME="_stealthy"
         break;
     else
         printf 'Sorry, you need to enter either "yes" or "no"\n'
@@ -36,6 +38,7 @@ while true ; do
 
     if [ "$VERBOSE" == "yes" ] || [ "$VERBOSE" == "y" ]; then
         VERBOSE_FLAG="-vv"
+        COMP_NAME.="_verbose"
         break;
     elif [ "$VERBOSE" == "no" ] || [ "$VERBOSE" == "n" ]; then
         VERBOSE_FLAG=""
@@ -45,6 +48,6 @@ while true ; do
     fi
 done
 
-echo -n "[+] Procceeding to aggresively scan the open ports..."
-nmap "$AGGRESIVE_FLAG" "$VERBOSE_FLAG" -oN "${NAME}_initial.scan" -p"$PORTS" "$IP"
+echo -n "[+] Procceeding to scan the open ports with your settings..."
+nmap "$AGGRESIVE_FLAG" "$VERBOSE_FLAG" -oN "${NAME}_${COMP_NAME}.scan" -p"$PORTS" "$IP"
 
